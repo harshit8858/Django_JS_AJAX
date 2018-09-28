28aa1c9b0b12eb336a9829803b72cbb6492b5c0e
harshit verma <harshit8858@gmail.com>
2018-09-28 11:06:52 +0530

    ajax update-jq&js

--- js/views.py
+++ js/views.py
@@ -3,6 +3,8 @@ from django.http import  JsonResponse, HttpResponse
 from .models import *
 from django.db.models import Q
 from .forms import *
+from django.template.loader import render_to_string
+from django.core import serializers
 
 
 def index(request):
@@ -15,14 +17,26 @@ def index(request):
 
 
 def add(request):
+    data = Data.objects.all()
+    data_json =serializers.serialize('json',data)
     if request.method == 'POST':
         form = TableForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('')
+            # if request.is_ajax():
+            #     print("hi")
+            #     html = render_to_string('js/add.html', {'form':form}, request=request)
+            #     return JsonResponse({'form':html})
+            # html = render_to_string('js/add.html', {'form': form}, request=request)
+            # return JsonResponse({'form':html})
+            return JsonResponse(data_json,safe=False)
+            # return HttpResponse(data_json)
+            # return JsonResponse('hi',safe=False)
+            # return redirect('js:add')
+
     else:
         form = TableForm()
-    return render(request, 'js/add.html', {'form':form})
+    return render(request, 'js/add.html', {'form':form, 'data':data})
 
 
 def search(request):
