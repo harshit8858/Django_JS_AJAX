083275850df568eb13fe1de1d500cd7a9f86855e
harshit verma <harshit8858@gmail.com>
2018-09-28 12:49:29 +0530

    Datatable enabled

--- js/views.py
+++ js/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from .forms import *
 from django.template.loader import render_to_string
 from django.core import serializers
+import json
 
 
 def index(request):
@@ -19,6 +20,13 @@ def index(request):
 def add(request):
     data = Data.objects.all()
     data_json =serializers.serialize('json',data)
+    data_dumps = json.dumps(data_json)
+    # print('data')
+    # print(data)
+    # print('data_json')
+    # print(data_json)
+    # print('data_dumps')
+    # print(data_dumps)
     if request.method == 'POST':
         form = TableForm(request.POST)
         if form.is_valid():
@@ -29,8 +37,8 @@ def add(request):
             #     return JsonResponse({'form':html})
             # html = render_to_string('js/add.html', {'form': form}, request=request)
             # return JsonResponse({'form':html})
-            return JsonResponse(data_json,safe=False)
-            # return HttpResponse(data_json)
+            # return JsonResponse(data_json,safe=False)
+            return HttpResponse(data_json, content_type="application/json")
             # return JsonResponse('hi',safe=False)
             # return redirect('js:add')
 
@@ -42,11 +50,12 @@ def add(request):
 def search(request):
     if request.method == 'POST':
         s = request.POST['search']
-        if s:
-            sa = Data.objects.filter(Q(name__icontains=s)|Q(number__icontains=s))
-            if sa:
-                return render(request, 'js/search.html', {'data': sa,'t':s})
-            else:
-                return render(request, 'notfound.html')
-        else:
-            return redirect('js:index')
+        # if s:
+        sa = Data.objects.filter(Q(name__icontains=s)|Q(number__icontains=s))
+            # if sa:
+            #     return render(request, 'js/search.html', {'data': sa,'t':s})
+            # else:
+            #     return render(request, 'notfound.html')
+        return render(request, 'js/search.html', {'data': sa, 't': s})
+        # else:
+        #     return redirect('js:index')
